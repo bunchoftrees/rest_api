0d3ec05ca2691b532d6820795c71dcf6b73d3c39
bunchoftrees <forrest@forrestbear.org>
2018-10-27 14:07:22 -0400

    final commit of excersise
    plan on actually making this work as expected at some point

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # rest_api
+A test of interacting with a server RESTfully
+
+POST: a 'block' of data up to 64MiB gets POSTed to the server and returns a SHA1 hash
+
+GET: given a SHA1 hash, returns block previously POSTed
+
+Assumption is that the hashing will happen on the server side, and only return previously posted, or applicable error states
+
+Error states should include:
+* Disk is full?
+* Data not found 404
+* Data corrupt on disk?
+* Data already exists?
+
+Unsure how to handle 'blocks', replacing with json. 
+
+Returning with SHA1 didn't work as expected, not enough time to find a fix
--- main.go
+++ main.go
@@ -1,33 +1,57 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
+type Data_block struct {
+	ID    string   `json:"id,omitempty"`
+	Block string   `json:"data_block,omitempty"`
+}
 
-func GetAllData(w http.ResponseWriter, r *http.Request) {}
-func GetData(w http.ResponseWriter, r *http.Request) {}
-func CreateData(w http.ResponseWriter, r *http.Request) {}
+var data_blocks []Data_block
+
+func GetData(w http.ResponseWriter, r *http.Request) {params := mux.Vars(r)
+	for _, item := range data_blocks {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Data_block{})}
+
+func CreateData(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var block Data_block
+	_ = json.NewDecoder(r.Body).Decode(&block)
+	block.ID = params["id"]
+	data_blocks = append(data_blocks, block)
+	json.NewEncoder(w).Encode(data_blocks)
+	return
+	//this doesn't work as expected
+	HashThis(data_blocks)
+
+
+}
+//hash function untested
+func HashThis(arr []Data_block) [20]byte {
+	arrBytes := []byte{}
+	for _, item := range arr {
+		jsonBytes, _ := json.Marshal(item)
+		arrBytes = append(arrBytes, jsonBytes...)
+	}
+	return sha1.Sum(arrBytes)
+}
 
 
 // main function
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/people", GetAllData).Methods("GET")
-	router.HandleFunc("/people/{id}", GetData).Methods("GET")
-	router.HandleFunc("/people/{id}", CreateData).Methods("POST")
-	log.Fatal(http.ListenAndServe(":8081", router))
+	data_blocks = append(data_blocks, Data_block{ID: "1", Block: "Forrest"})
+	router.HandleFunc("/data/{id}", GetData).Methods("GET")
+	router.HandleFunc("/data/{id}", CreateData).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8082", router))
 }
-
-type Data struct {
-	ID        string   `json:"id,omitempty"`
-	Data_type string   `json:"data_type,omitempty"`
-	Hash string `json:"hash,omitempty"`
-}
-type Address struct {
-	City  string `json:"city,omitempty"`
-	State string `json:"state,omitempty"`
-}
-
-var people []Person
